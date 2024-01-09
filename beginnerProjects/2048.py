cc5b89a478373631219668fcc38e3c90ac5948cd
unknown <karkisanjay2002@gmail.com>
2024-01-09 18:26:42 +0545

    Apply while loop until the cell value is not zero to generate random r and c values using random.randint, if the cell value is zero, assign 2 to it

--- beginnerProjects/2048.py
+++ beginnerProjects/2048.py
@@ -3,77 +3,81 @@ import random
 print('*'*30)
 print("Welcome to the 2048 game.")
 print('*'*30)
-
-def generateRandNums(size):
+#put 2 on random cell on matrix
+def generateRandNums(arr,size,value):
     x= random.randint(0,size)
     y = random.randint(0, size)
-    p = random.randint(0, size)
-    q = random.randint(0, size)
-    return  x,y,p,q
-
+    while (arr[x][y] != 0):
+        x= random.randint(0, size)
+        y= random.randint(0, size)
+    arr[x][y] =value
+def generateMatrx(size):
+    arr = [[0 for i in range(size)] for j in range(size)]
+    return arr
 def generateInitialMatrix(size):
-   arr = [[0 for i in range(size)] for  j in range(size)]
-   #x,y,p,q = generateRandNums(size-1)
-   if arr[0][0] == 0 and arr[0][1]==0:
-
-       arr[0][1]=2
-
-       arr[0][2] = 2
-       arr[1][3] = 2
-       arr[1][0] = 2
+   arr = generateMatrx(size)
+   generateRandNums(arr,size-1,2)
+   generateRandNums(arr,size-1,2)
    return arr
 
-def addValuesMatrix(arr,state):
-    if state =='a':
-        for i,row in enumerate(arr):
-            for j in range(len(arr)):
-              if row[i][j] == row[i][j+1]:
-                  row[i][j] *= 2
-    return  arr
+def addLeft(arr,addarr,idxsarr,size):
 
+    for i in range(size):
+        for j in range(size):
+            #print(j% 3, (j%3)+1)# 0 1 1 2 2 3 0 1
+            if j == 0:
+                  if addarr[i][0] == addarr[i][1] and addarr[i][0] != 0 and addarr[i][1] != 0:
+                        print(f"1: , {addarr[i][0]}:{i,0},{addarr[i][1]}:{i,1}")
+                        c1 =idxsarr[i][0]
+                        c2 = idxsarr[i][1]
+                        arr[i][c1] *= 2
+                        arr[i][c2] = 0
 
-
-arr=generateInitialMatrix(4)
-#updateArr=addValuesMatrix(arr,'a')
-# for i in arr:
-#     print(i)
-def addLeft(arr):
-    for i in range(4):
-        idces=[]
-        for j in range(4):
+            elif (j%3== 0 and j!=0):
+                continue
+            else:
+               if addarr[i][j % 3] == addarr[i][j % 3+ 1] and addarr[i][j % 3] != 0 and addarr[i][j % 3 +1] != 0 :
+                    print(f"3: ,{addarr[i][j % 3]}:{i,j % 3},{addarr[i][j % 3+1]}:{i,j % 3+1}")
+                    c1 = idxsarr[i][j % 3]
+                    c2 = idxsarr[i][j % 3+1]
+                    arr[i][c1] *= 2
+                    arr[i][c2] = 0
+    return arr
+def addTwoValues(arr,size):
+    valuearr= generateMatrx(4)
+    idxarr = generateMatrx(4)
+    for i in range(size):
+        count=0
+        for j in range(size):
             if arr[i][j] != 0:
-                idces.append(j)
-        if len(idces)>=2:
-            print(idces)
-            for k in range(len(idces)):
-              if arr[i][idces[k-1]] == arr[i][idces[k]]:
-                     arr[i][idces[k]] *=2
-                     arr[i][idces[k-1]] =0
-    x,y,_,_=generateRandNums(3)
-    if arr[x][y] == 0:
-        arr[x][y] =2
+                valuearr[i][count] = arr[i][j]
+                idxarr[i][count] =j
+                count+=1
+            if count < size:
+              idxarr[i][count] = -1
 
-
-    return  arr
-
-newarr=addLeft(arr)
-
-newarr[0][2] = 2
-newarr[0][3]=2
-newarr1 = newarr[:]
-newarr1=addLeft(newarr1)
-# newarr2=addLeft(newarr1)
-
-
-
-
-
+    return valuearr,idxarr
+# arr=generateInitialMatrix(4)
+# generateRandNums(arr,3,2)
+# generateRandNums(arr,3,3)
+arr = generateMatrx(4)
+arr[0][0]=8
+arr[0][1]=0
+arr[0][2]=6
+arr[0][3]=8
 for i in arr:
     print(i)
 print("\n")
+newarr,idxarr= addTwoValues(arr,4)
 for i in newarr:
     print(i)
-# # print("\n")
-# for i in newarr2:
-#     print(i)
-    
+
+arr= addLeft(arr,newarr,idxarr, 4)
+# print("\n")
+for i in idxarr:
+    print(i)
+print("\n")
+for i in arr:
+    print(i)
+
+
